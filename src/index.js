4e0d4ee00a50d6223adf3ec6adb3a8ff2154bcfb
vsr4493@gmail.com <vsr4493@gmail.com>
2017-10-14 20:42:00 +0530

    Added Bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+//Adding Bootstrap
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
